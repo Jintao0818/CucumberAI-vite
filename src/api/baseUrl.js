be98b94ddb5e2a271d10bc1dc43f63eaa98efc2e
Jintao0818 <2784666083@qq.com>
2023-09-04 00:19:22 +0800

    mod:main instruction

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 let baseUrl = ''
 switch (import.meta.env.MODE) {
   case 'development':
-    baseUrl = 'http://192.168.1.100:3333' //开发环境
+    baseUrl = 'http://192.168.31.144:3333' //开发环境
     break
   case 'production':
     baseUrl = 'http://47.100.80.240:9568/' //生产环境
